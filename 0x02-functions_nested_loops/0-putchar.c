64a0a3a4cb7fdadfade8fc690f91fa87101b0701
codewithAntony <antonymurithi51@gmail.com>
2023-05-23 14:45:01 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - print _putchar
- * * is written by murithi
- * Return: 0
- */
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
 
 
 int main(void)
@@ -17,5 +17,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0);    
+return (0);
 }
+
